3e84305bd5912b48f3138fc0326045e5a67d3224
Donatas <donatas.gostautas@gjensidige.no>
2024-05-17 16:25:42 +0300

    feat: trying out ARIMA

--- src/dogotry.py
+++ src/dogotry.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
+from statsmodels.tsa.arima.model import ARIMA
+
 
 
 def prepare_data():
@@ -49,13 +51,33 @@ def apply_model(model: Any):
     logger.success(f"Predicted population count: {prediction[0]}")
 
 
+def test_arima_model():
+    # Prepare the data
+    X_train, X_test, y_train, y_test = prepare_data()
+
+    # Train the model
+    model = ARIMA(y_train, order=(5, 1, 0))
+    model_fit = model.fit(disp=0)
+
+    # Evaluate the model
+    predictions = model_fit.forecast(steps=len(y_test))[0]
+    logger.success(f"Model: {model.__class__.__name__}")
+    logger.success(f"RMSE: {root_mean_squared_error(y_test, predictions)}")
+
+    return model_fit
+
+
 def test():
     apply_model(LinearRegression())
     apply_model(DecisionTreeRegressor())
     apply_model(RandomForestRegressor())
-    apply_model(GradientBoostingRegressor())
+    apply_model(
+        GradientBoostingRegressor(
+            n_estimators=100, learning_rate=0.1, max_depth=1, random_state=0, loss="ls"
+        )
+    )
     apply_model(SVR())
-    apply_model(MLPRegressor())
+    apply_model(MLPRegressor(max_iter=500))
 
 
 def generate_results():
@@ -71,4 +93,5 @@ def generate_results():
 
 
 if __name__ == "__main__":
-    generate_results()
+    # generate_results()
+    test_arima_model()
